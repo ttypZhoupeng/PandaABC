08ec426d3b2203ac39cc603d513dc2256d590ce1
zhoupeng520 <2460155936@example.com>
2017-12-11 10:11:02 +0800

    静态完成

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // 监听滚动条，改变top样式
 $(function(){
+    // 导航条
     $(window).scroll(function(){
         if($(window).scrollTop()>"150"){
             $('header .top').addClass('change-top');
@@ -7,4 +8,31 @@ $(function(){
             $('header .top').removeClass('change-top');
         }
     })
+
+    // 二位码弹出
+    $('.button').click(function(){
+        $('.modal').fadeIn(800);
+    })
+    $('.modal').click(function(){
+        $(this).fadeOut(800);
+    })
+
+    // swiper 轮播
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 3,
+        centeredSlides: true,
+        autoplay: 3000,
+        loop: true,
+        slideToClickedSlide: true,
+        paginationClickable: true,
+        spaceBetween: 30,
+        breakpoints: {
+          // when window width is <= 320px
+          767: {
+            slidesPerView: 1,
+            spaceBetween: 10
+          }
+        }
+      });   
 })
